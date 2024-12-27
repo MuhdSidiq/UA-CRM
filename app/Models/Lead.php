2a9dafed9a7a344340dfe085433567a64931cfa7
Muhammad Sidiq <Muhammad>
2024-12-27 11:11:10 +0800

    Lead table

--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -2,39 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Lead extends Model
 {
-    use HasFactory;
-
     //
-
-    // Relationship with TelegramAccount
-    public function telegramAccount(): BelongsTo
-    {
-        return $this->belongsTo(TelegramAccount::class);
-    }
-
-    // Relationship with ChatLogs
-    public function chatLogs(): HasMany
-    {
-        return $this->hasMany(ChatLog::class);
-    }
-
-    // Relationship with ChatStatusLogs
-    public function statusLogs(): HasMany
-    {
-        return $this->hasMany(ChatStatusLog::class);
-    }
-
-    // Relationship with LeadMeta
-    public function meta(): HasOne
-    {
-        return $this->hasOne(LeadMeta::class);
-    }
 }
---
+++ database/migrations/2024_12_26_133104_create_leads_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leads', function (Blueprint $table) {
+            $table->id();
+            $table->integer('telegram_account_id')->nullable();
+            $table->string('telegram_chat_id')->nullable();
+            $table->string('telegram_username')->nullable();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('country')->nullable();
+            $table->enum('status', ['new', 'f1', 'f2', '50D', 'close', 'cold lead'])->default('new');
+            $table->string('username')->nullable();
+            $table->string('platform')->nullable();
+            $table->timestamp('first_message_date')->nullable();
+            $table->timestamp('last_message_date')->nullable();
+            $table->timestamps();
+
+            // Foreign key
+            $table->foreign('telegram_account_id')
+                ->references('id')
+                ->on('telegram_accounts')
+                ->nullOnDelete();
+
+            // Indexes
+            $table->index('telegram_account_id', 'idx_telegram_account');
+            $table->index('status', 'idx_status');
+            $table->index(['first_message_date', 'last_message_date'], 'idx_dates');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leads');
+    }
+};
